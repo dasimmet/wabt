98a0e4aad56f5b14560a681cdcb402af020918fd
Tobias Simetsreiter <dasimmet@gmail.com>
2025-01-07 23:20:12 +0100

    working version

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
-# [WebAssembly Binary Toolkit](https://github.com/WebAssembly/wabt.git) on the zig build system
+# WebAssembly Tools on the zig build system
 
-this builds the [WebAssembly Binary Toolkit](https://github.com/WebAssembly/wabt.git) to
-use on the [Zig](https://ziglang.org/) build system
+uses the [Zig](https://ziglang.org/) build system to build WebAssembly's official
+C tools.
 
-# add to your project
+## add to your zig project
 
 ```bash
 zig fetch --save git+https://github.com/dasimmet/wabt.git
 ```
 
-# build.zig usage
+## [WebAssembly Binary Toolkit](https://github.com/WebAssembly/wabt.git)
+
+```zig
+
+```
+
+## [Binaryen](https://github.com/WebAssembly/binaryen.git)
+
+
+## build.zig usage
 
 ```zig
 const wabt = @import("wabt");
-const my_wat: LazyPath = wabt.wasm2wat(b.path("my.wasm"), "my.wat");
+const my_wat: LazyPath = wabt.wasm2wat(
+    b.path("my.wasm"), // source path
+    "my.wat", // out_basename
+);
 ```
--- binaryen.zig
+++ binaryen.zig
@@ -1,15 +1,38 @@
 const std = @import("std");
 const LazyPath = std.Build.LazyPath;
+const Dependency = std.Build.Dependency;
+
+const binaryen_version_h = "121";
 
 pub fn build(b: *std.Build, target: std.Build.ResolvedTarget, opt: std.builtin.OptimizeMode) void {
-    const src_dep = b.dependency("binaryen", .{});
+    if (b.lazyDependency("binaryen", .{})) |src_dep| {
+        buildLazy(b, src_dep, target, opt);
+    }
+}
+
+pub fn buildLazy(
+    b: *std.Build,
+    src_dep: *Dependency,
+    target: std.Build.ResolvedTarget,
+    opt: std.builtin.OptimizeMode,
+) void {
     const config_h = b.addConfigHeader(.{
         .style = .{ .cmake = src_dep.path("config.h.in") },
         .include_path = "config.h",
     }, .{
-        .PROJECT_VERSION = "121",
+        .PROJECT_VERSION = binaryen_version_h,
     });
     const config_h_include = config_h.getOutput().dirname();
+
+    const intrinsics_basename = "WasmIntrinsics.cpp";
+    const intrinsics_src = b.addConfigHeader(.{
+        .style = .{ .cmake = src_dep.path("src/passes/WasmIntrinsics.cpp.in") },
+        .include_path = intrinsics_basename,
+    }, .{
+        .WASM_INTRINSICS_EMBED = "0x00",
+    });
+    const intrinsics = intrinsics_src.getOutput().dirname();
+
     const lib = b.addStaticLibrary(.{
         .name = "binaryen",
         .target = target,
@@ -17,16 +40,29 @@ pub fn build(b: *std.Build, target: std.Build.ResolvedTarget, opt: std.builtin.O
     });
     const src_path = src_dep.path("src");
     const fp16_path = src_dep.path("third_party/FP16/include");
-    const llvm_path = src_dep.path("third_party/llvm-project/include");
     lib.linkLibCpp();
     lib.addIncludePath(config_h_include);
     lib.addIncludePath(src_path);
     lib.addIncludePath(fp16_path);
-    lib.addIncludePath(llvm_path);
+    lib.addIncludePath(src_dep.path(llvm.include_path));
     lib.addCSourceFiles(.{
         .files = lib_sources,
         .root = src_path,
     });
+    b.installArtifact(lib);
+
+    const llvm_lib = b.addStaticLibrary(.{
+        .name = "LLVMDWARF",
+        .target = target,
+        .optimize = opt,
+    });
+    llvm_lib.linkLibCpp();
+    llvm_lib.addCSourceFiles(.{
+        .files = llvm.sources,
+        .root = src_dep.path(llvm.src_path),
+    });
+    llvm_lib.addIncludePath(src_dep.path(llvm.include_path));
+
     inline for (dep_libs) |dep_lib| {
         const d_lib = b.addStaticLibrary(.{
             .name = "binaryen_" ++ dep_lib.path,
@@ -39,16 +75,24 @@ pub fn build(b: *std.Build, target: std.Build.ResolvedTarget, opt: std.builtin.O
             .files = dep_lib.sources,
             .root = d_root,
         });
+        if (ptrHasField(dep_lib, "add_intrinsics")) {
+            d_lib.addCSourceFiles(.{
+                .files = &.{intrinsics_basename},
+                .root = intrinsics,
+            });
+        }
+        if (ptrHasField(dep_lib, "link_llvm")) {
+            d_lib.addIncludePath(src_dep.path(llvm.include_path));
+        }
         d_lib.addIncludePath(src_path);
         d_lib.addIncludePath(config_h_include);
         d_lib.addIncludePath(fp16_path);
-        d_lib.addIncludePath(llvm_path);
         lib.linkLibrary(d_lib);
         b.step("binaryen-lib-" ++ dep_lib.path, "binaryen-library").dependOn(&d_lib.step);
     }
 
     const tools_path = src_dep.path("src/tools");
-    const tools_fuzzing_path = src_dep.path("src/tools/fuzzing");
+    const tools_fuzzing_path = src_dep.path(tools_fuzzing.path);
     inline for (tools) |t| {
         const exe = b.addExecutable(.{
             .name = t,
@@ -65,40 +109,46 @@ pub fn build(b: *std.Build, target: std.Build.ResolvedTarget, opt: std.builtin.O
             .root = tools_path,
         });
         exe.addCSourceFiles(.{
-            .files = tools_fuzzing,
+            .files = tools_fuzzing.sources,
             .root = tools_fuzzing_path,
         });
+        exe.linkLibrary(llvm_lib);
         b.installArtifact(exe);
     }
 }
 
+fn ptrHasField(ptr: anytype, comptime name: []const u8) bool {
+    const ptr_type = @typeInfo(@TypeOf(ptr)).pointer.child;
+    return @hasField(ptr_type, name);
+}
+
 const tools = &.{
-    // "wasm-as",
-    // "wasm-ctor-eval",
-    // "wasm-dis",
-    // "wasm-emscripten-finalize",
-    // "wasm-fuzz-lattices",
-    // "wasm-fuzz-types",
-    // "wasm-merge",
-    // "wasm-metadce",
+    "wasm-as",
+    "wasm-ctor-eval",
+    "wasm-dis",
+    "wasm-emscripten-finalize",
+    "wasm-fuzz-lattices",
+    "wasm-fuzz-types",
+    "wasm-merge",
+    "wasm-metadce",
     "wasm-opt",
-    // "wasm-reduce",
-    // "wasm-shell",
+    "wasm-reduce",
+    "wasm-shell",
 };
 
-const tools_fuzzing = &.{
-    "fuzzing.cpp",
-    "heap-types.cpp",
-    "random.cpp",
+const tools_fuzzing = .{
+    .path = "src/tools/fuzzing",
+    .sources = &.{
+        "fuzzing.cpp",
+        "heap-types.cpp",
+        "random.cpp",
+    },
 };
 
 const lib_sources = &.{
     "analysis/cfg.cpp",
     "binaryen-c.cpp",
     "cfg/Relooper.cpp",
-    // "tools/fuzzing/fuzzing.cpp",
-    // "tools/fuzzing/heap-types.cpp",
-    // "tools/fuzzing/random.cpp",
 };
 
 const dep_libs = &.{
@@ -168,6 +218,7 @@ const libwasm = .{
 
 const libsupport = .{
     .path = "support",
+    .link_llvm = true,
     .sources = &.{
         "archive.cpp",
         "bits.cpp",
@@ -191,6 +242,8 @@ const libsupport = .{
 
 const libpasses = .{
     .path = "passes",
+    .add_intrinsics = true,
+    .link_llvm = true,
     .sources = &.{
         "AbstractTypeRefining.cpp",
         "AlignmentLowering.cpp",
@@ -339,5 +392,75 @@ const libir = .{
         "stack-utils.cpp",
         "table-utils.cpp",
         "type-updating.cpp",
+    },
+};
+
+const llvm = .{
+    .src_path = "third_party/llvm-project",
+    .include_path = "third_party/llvm-project/include",
+    .sources = &.{
+        "Binary.cpp",
+        "ConvertUTF.cpp",
+        "DataExtractor.cpp",
+        "Debug.cpp",
+        "DJB.cpp",
+        "Dwarf.cpp",
+        "dwarf2yaml.cpp",
+        "DWARFAbbreviationDeclaration.cpp",
+        "DWARFAcceleratorTable.cpp",
+        "DWARFAddressRange.cpp",
+        "DWARFCompileUnit.cpp",
+        "DWARFContext.cpp",
+        "DWARFDataExtractor.cpp",
+        "DWARFDebugAbbrev.cpp",
+        "DWARFDebugAddr.cpp",
+        "DWARFDebugAranges.cpp",
+        "DWARFDebugArangeSet.cpp",
+        "DWARFDebugFrame.cpp",
+        "DWARFDebugInfoEntry.cpp",
+        "DWARFDebugLine.cpp",
+        "DWARFDebugLoc.cpp",
+        "DWARFDebugMacro.cpp",
+        "DWARFDebugPubTable.cpp",
+        "DWARFDebugRangeList.cpp",
+        "DWARFDebugRnglists.cpp",
+        "DWARFDie.cpp",
+        "DWARFEmitter.cpp",
+        "DWARFExpression.cpp",
+        "DWARFFormValue.cpp",
+        "DWARFGdbIndex.cpp",
+        "DWARFListTable.cpp",
+        "DWARFTypeUnit.cpp",
+        "DWARFUnit.cpp",
+        "DWARFUnitIndex.cpp",
+        "DWARFVerifier.cpp",
+        "DWARFVisitor.cpp",
+        "DWARFYAML.cpp",
+        "Error.cpp",
+        "ErrorHandling.cpp",
+        "FormatVariadic.cpp",
+        "Hashing.cpp",
+        "LEB128.cpp",
+        "LineIterator.cpp",
+        "MCRegisterInfo.cpp",
+        "MD5.cpp",
+        "MemoryBuffer.cpp",
+        "NativeFormatting.cpp",
+        "ObjectFile.cpp",
+        "obj2yaml_Error.cpp",
+        "Optional.cpp",
+        "Path.cpp",
+        "raw_ostream.cpp",
+        "ScopedPrinter.cpp",
+        "SmallVector.cpp",
+        "SourceMgr.cpp",
+        "StringMap.cpp",
+        "StringRef.cpp",
+        "SymbolicFile.cpp",
+        "Twine.cpp",
+        "UnicodeCaseFold.cpp",
+        "WithColor.cpp",
+        "YAMLParser.cpp",
+        "YAMLTraits.cpp",
     },
 };
